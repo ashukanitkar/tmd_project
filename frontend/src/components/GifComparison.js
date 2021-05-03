14203aaaebf6fcb4c18868b28b1b0c9d82b96fd4
ashukanitkar <ak2729@cornell.edu>
2021-05-02 22:53:38 -0400

    completes paginated card functionality on gif comp

--- frontend/src/components/GifComparison.js
+++ frontend/src/components/GifComparison.js
@@ -1,4 +1,3 @@
-import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
@@ -6,7 +5,9 @@ import IconButton from '@material-ui/core/IconButton';
 import Grid from '@material-ui/core/Grid';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
-import pug1 from '../assets/pug1.jpeg'
+import pug1 from '../assets/pug1.jpeg';
+import pug2 from '../assets/pug2.jpeg';
+import pug3 from '../assets/pug3.jpeg';
 import React from 'react';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import { withStyles } from "@material-ui/core/styles";
@@ -25,6 +26,29 @@ class GifComparison extends React.Component {
   
   constructor(props) {
     super(props);
+    this.state = {
+      current_image_index: 0,
+      current_image: pug1
+    }
+    this.arrowClick = this.arrowClick.bind(this);
+  }
+
+  pug_images = [pug1, pug2, pug3]
+  num_images = this.pug_images.length
+
+  arrowClick(event) {
+    if (event.currentTarget.id == "forward") {
+      this.setState({
+        current_image_index: Math.min(this.state.current_image_index + 1, this.pug_images.length-1),
+        current_image: this.pug_images[Math.min(this.state.current_image_index + 1, this.pug_images.length-1)]
+      })
+      
+    } else {
+      this.setState({
+        current_image_index: Math.max(this.state.current_image_index - 1, 0),
+        current_image: this.pug_images[Math.max(this.state.current_image_index - 1, 0)]
+      })
+    }
   }
 
   render() {
@@ -36,18 +60,18 @@ class GifComparison extends React.Component {
               component="img"
               alt="Pug1"
               height="350"
-              image={pug1}
+              image={this.state.current_image}
               title="Pug"
             />
           </CardActionArea>
           <CardContent>
             <div className={classes.controls}>
             <Grid container justify="center">
-                <IconButton aria-label="previous">
-                  {theme.direction === 'rtl' ? <ArrowRightIcon /> : <ArrowLeftIcon />}
+                <IconButton aria-label="previous" onClick={this.arrowClick} id="backward" disabled={this.state.current_image_index == 0}>
+                  <ArrowLeftIcon />
                 </IconButton>
-                <IconButton aria-label="next">
-                  {theme.direction === 'rtl' ? <ArrowLeftIcon /> : <ArrowRightIcon />}
+                <IconButton aria-label="next" onClick={this.arrowClick} id="forward" disabled={this.state.current_image_index == this.num_images-1}>
+                  <ArrowRightIcon />
                 </IconButton>
             </Grid>
             </div>
