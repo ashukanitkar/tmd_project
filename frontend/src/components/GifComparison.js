d52f7f66a0570c36cf1ca9b9b26dfbbdd6ec8859
Ashu <61364492+ashukanitkar@users.noreply.github.com>
2021-05-03 08:53:40 -0400

    Conditionally Renders Gif Component (#1)
    * converts app to class and adds conditional rendering
    
    * removes commented out code
    
    * includes skeleton card for gif component w image
    
    * completes paginated card functionality on gif comp

--- frontend/src/components/GifComparison.js
+++ frontend/src/components/GifComparison.js
@@ -1,7 +1,88 @@
-import React from "react";
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
+import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import ArrowLeftIcon from '@material-ui/icons/ArrowLeft';
+import pug1 from '../assets/pug1.jpeg';
+import pug2 from '../assets/pug2.jpeg';
+import pug3 from '../assets/pug3.jpeg';
+import React from 'react';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import { withStyles } from "@material-ui/core/styles";
 
-export default function GifComparison() {
-  return (
-      <div>gif comparison component</div>
-  )
+const styles = theme => ({
+  root: {
+    width: "25%",
+  },
+  controls: {
+        alignItems: 'center',
+        display: 'flex'
+  }
+});
+
+class GifComparison extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      current_image_index: 0,
+      current_image: pug1
+    }
+    this.arrowClick = this.arrowClick.bind(this);
+  }
+
+  pug_images = [pug1, pug2, pug3]
+  num_images = this.pug_images.length
+
+  arrowClick(event) {
+    if (event.currentTarget.id == "forward") {
+      this.setState({
+        current_image_index: Math.min(this.state.current_image_index + 1, this.pug_images.length-1),
+        current_image: this.pug_images[Math.min(this.state.current_image_index + 1, this.pug_images.length-1)]
+      })
+      
+    } else {
+      this.setState({
+        current_image_index: Math.max(this.state.current_image_index - 1, 0),
+        current_image: this.pug_images[Math.max(this.state.current_image_index - 1, 0)]
+      })
+    }
+  }
+
+  render() {
+        const { classes, theme } = this.props;
+        return (
+        <Card className={classes.root} variant="outlined">
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              alt="Pug1"
+              height="350"
+              image={this.state.current_image}
+              title="Pug"
+            />
+          </CardActionArea>
+          <CardContent>
+            <div className={classes.controls}>
+            <Grid container justify="center">
+                <IconButton aria-label="previous" onClick={this.arrowClick} id="backward" disabled={this.state.current_image_index == 0}>
+                  <ArrowLeftIcon />
+                </IconButton>
+                <IconButton aria-label="next" onClick={this.arrowClick} id="forward" disabled={this.state.current_image_index == this.num_images-1}>
+                  <ArrowRightIcon />
+                </IconButton>
+            </Grid>
+            </div>
+          </CardContent>
+        </Card>
+      );
+    }
 }
+
+export default withStyles(styles, { withTheme: true })(GifComparison);
+
+
+
+
