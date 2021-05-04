ff602a16a017e2a700a06ceb34257be5f9673d0b
ashukanitkar <ak2729@cornell.edu>
2021-05-04 14:56:55 -0400

    first pass at refactoring playlists to be carousel

--- frontend/src/components/Playlists.js
+++ frontend/src/components/Playlists.js
@@ -1,20 +1,70 @@
-import React from "react";
-import {Redirect, withRouter} from "react-router-dom";
-import Button from "@material-ui/core/Button";
-
-// TODO - change to actual watch history file later, using small file for now
+import React from 'react';
+import Carousel from 'react-material-ui-carousel';
+import autoBind from 'auto-bind';
+// // TODO - change to actual watch history file later, using small file for now
 import jsonData from '../watch_histories_small.json'
-import {Typography} from "@material-ui/core";
+import { withStyles } from "@material-ui/core/styles";
+
+import {
+    Paper,
+    Button,
+    Typography
+} from '@material-ui/core'
+
+const styles = theme => ({
+    root: {
+      width: "25%",
+    },
+    SecondExample: {
+        width: "50%",
+        margin: "auto"
+    }
+  });
+  
+function Project(props) {
+    return (
+        <Paper
+            className="Project"
+            style={{
+                width: "50%",
+                margin: "auto",
+                textAlign: "center"
+            }}
+            elevation={10}
+        >
+            <div 
+            style={{
+                margin: "auto"
+            }}>
+                 <Typography>{props.item.title}</Typography>
+                 <iframe width="340" height="200" src={props.item.link}
+                    title="YouTube video player" frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen>
+                </iframe>
+            </div>
+        </Paper>
+    )
+}
 
 class Playlists extends React.Component {
     constructor(props) {
         super(props);
+
         this.state = {
-            name: "React",
+            autoPlay: false,
+            animation: "slide",
+            indicators: true,
+            timeout: 500,
+            navButtonsAlwaysVisible: true,
+            navButtonsAlwaysInvisible: false,
             selectedOption: null
-        };
+        }
+        
         this.onValueChange = this.onValueChange.bind(this);
         this.formSubmit = this.formSubmit.bind(this);
+
+        autoBind(this);
     }
 
     onValueChange(event) {
@@ -39,57 +89,45 @@ class Playlists extends React.Component {
     }
 
     render() {
-        const renderGifs = () => {
-            if (this.state.isSubmitted) {
-                return (
-                    <div>
-                        <Redirect
-                            to={{
-                                pathname: "compare",
-                                state: {playlist: this.state.selectedOption}
-                            }}
-                        />
-                    </div>
-                )
-            } else {
-                const loadData = () => JSON.parse(JSON.stringify(jsonData));
-                let playlists = []
-                Object.keys(loadData().users).forEach((key) => {
-                    let user = []
-                    loadData().users[key].videos.forEach((video) => {
-                        user.push(video);
-                    });
-                    playlists.push(user);
-                })
+        const { classes, theme } = this.props;
+        const loadData = () => JSON.parse(JSON.stringify(jsonData));
+        let playlists = []
+        Object.keys(loadData().users).forEach((key) => {
+            let user = []
+            loadData().users[key].videos.forEach((video) => {
+                user.push(video);
+            });
+            playlists.push(user);
+        })
+        return (
+            <div style={{ marginTop: "50px", color: "#494949"}}>
+                {playlists.map((playlist, idx) => {
+                    return (
+                        <div>
+                            <h2>Playlist {idx + 1}</h2>
+                            <Carousel
+                                className={classes.SecondExample}
+                                autoPlay={this.state.autoPlay}
+                                animation={this.state.animation}
+                                indicators={this.state.indicators}
+                                timeout={this.state.timeout}
+                                navButtonsAlwaysVisible={this.state.navButtonsAlwaysVisible}
+                                navButtonsAlwaysInvisible={this.state.navButtonsAlwaysInvisible}
 
-                return (
-                    <div className="App">
-                        {playlists.map((playlist, idx) => {
-                            return (
-                                <div>
-                                    <h2>Playlist {idx + 1}</h2>
-                                    {playlist.map(video => {
-                                        return (
-                                            <div>
-                                                <Typography>{video.title}</Typography>
-                                                <iframe width="340" height="200" src={video.link}
-                                                        title="YouTube video player" frameBorder="0"
-                                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                                        allowFullScreen>
-                                                </iframe>
-                                            </div>
-                                        )
-                                    })}
-                                </div>
-                            )
-                        })}
+                            >
+                                {
+                                    playlist.map( (video => <Project item={video} /> ))
+                                }
+                            </Carousel>
 
-                        <h2>Which playlist would you watch?</h2>
+                        </div>
+                    )})}
+                    <h2>Which playlist would you watch?</h2>
 
-                        <form onSubmit={this.formSubmit}>
-                            <div className="radio">
-                                <label>
-                                    <input
+                         <form onSubmit={this.formSubmit}>
+                             <div className="radio">
+                                 <label>
+                                     <input
                                         type="radio"
                                         value="playlist1"
                                         checked={this.state.selectedOption === "playlist1"}
@@ -113,12 +151,8 @@ class Playlists extends React.Component {
                                 See Results
                             </Button>
                         </form>
-                    </div>
-                )
-            }
-        }
-        return (renderGifs())
+                </div>
+        )
     }
 }
-
-export default withRouter(Playlists)
+export default withStyles(styles, { withTheme: true })(Playlists);
