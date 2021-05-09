b7737287aad1778af6be7c374d82269a609b9f32
ashukanitkar <ak2729@cornell.edu>
2021-05-08 21:15:15 -0400

    added static word clouds for diff groups

--- frontend/src/components/Findings.js
+++ frontend/src/components/Findings.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import left_channels_wc from "../left_channels.png"
+import right_channels_wc from "../right_channels.png"
+import center_channels_wc from "../control_channels.png"
 
 class Findings extends React.Component {
     render() {
-        return <p>hi</p>
+        return (
+            <div>
+            <p>Left Channels</p>
+            <img src={left_channels_wc}/>
+            <p>Right Channels</p>
+            <img src={right_channels_wc}/>
+            <p> Control Channels </p>
+            <img src={center_channels_wc}/>
+            </div>
+        )
     }
 }
 
