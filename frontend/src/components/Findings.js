592f8aa4fddde943c2c93f11f9b2f56326aed7cf
ashukanitkar <ak2729@cornell.edu>
2021-05-08 23:49:42 -0400

    adds a barchart visualization for channels on findings page

--- frontend/src/components/Findings.js
+++ frontend/src/components/Findings.js
@@ -1,21 +1,109 @@
-import React from 'react';
-import left_channels_wc from "../left_channels.png"
-import right_channels_wc from "../right_channels.png"
-import center_channels_wc from "../control_channels.png"
+import { ResponsiveBar } from '@nivo/bar'
+import React from 'react'
+import {withStyles} from "@material-ui/core/styles";
+import data from './BarChart/data.js'
+import "./BarChart/chart.css"
 
 class Findings extends React.Component {
+
     render() {
         return (
-            <div>
-            <p>Left Channels</p>
-            <img src={left_channels_wc}/>
-            <p>Right Channels</p>
-            <img src={right_channels_wc}/>
-            <p> Control Channels </p>
-            <img src={center_channels_wc}/>
-            </div>
+            <div class="chart">
+                <ResponsiveBar
+                    data={data}
+                    keys={["left_users", "right_users", "control_users"]}
+                    indexBy="channel"
+                    margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+                    padding={0.3}
+                    valueScale={{ type: 'linear' }}
+                    indexScale={{ type: 'band', round: true }}
+                    colors={{ scheme: 'nivo' }}
+                    defs={[
+            {
+                id: 'dots',
+                type: 'patternDots',
+                background: 'inherit',
+                color: '#38bcb2',
+                size: 4,
+                padding: 1,
+                stagger: true
+            },
+            {
+                id: 'lines',
+                type: 'patternLines',
+                background: 'inherit',
+                color: '#eed312',
+                rotation: -45,
+                lineWidth: 6,
+                spacing: 10
+            }
+        ]}
+        fill={[
+            {
+                match: {
+                    id: 'fries'
+                },
+                id: 'dots'
+            },
+            {
+                match: {
+                    id: 'sandwich'
+                },
+                id: 'lines'
+            }
+        ]}
+        borderColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+        axisTop={null}
+        axisRight={null}
+        axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Channel',
+            legendPosition: 'middle',
+            legendOffset: 32
+        }}
+        axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: 'Frequency',
+            legendPosition: 'middle',
+            legendOffset: -40
+        }}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor={{ from: 'color', modifiers: [ [ 'darker', 1.6 ] ] }}
+        legends={[
+            {
+                dataFrom: 'keys',
+                anchor: 'bottom-right',
+                direction: 'column',
+                justify: false,
+                translateX: 120,
+                translateY: 0,
+                itemsSpacing: 2,
+                itemWidth: 100,
+                itemHeight: 20,
+                itemDirection: 'left-to-right',
+                itemOpacity: 0.85,
+                symbolSize: 20,
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemOpacity: 1
+                        }
+                    }
+                ]
+            }
+        ]}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
+    />
+    </div>
         )
     }
 }
-
-export default Findings
+export default Findings;
